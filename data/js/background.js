e3f406e21626f576533870735f73b8e8d66de4b3
yykny+925454ryx0ots@sharklasers.com <ribhav>
2015-01-19 21:11:34 +0530

    navbar1.3
    -customised it accr to designer
    -added logo and app store icon

--- data/js/background.js
+++ data/js/background.js
@@ -1,5 +1,10 @@
-var bgcount = 15;
+var bgcount = 2;
 function changebg() {
     var num =  Math.ceil( Math.random() * bgcount );
-    document.body.style.background = 'url(/freidae/data/backgrounds/antarctic'+num+'.jpg)';
+    document.body.style.background = 'url(/freidae/data/backgrounds/backgroundimage'+num+'.jpg)';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = '100% 100%';
+    document.body.style.backgroundOrigin = 'border-box';
+    document.body.style.backgroundAttachment = 'fixed';
+    document.body.style.backgroundPosition = 'center center';
 }
