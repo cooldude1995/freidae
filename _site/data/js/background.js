494234ad29c3335318003aa2be93e673475f657a
yykny+925454ryx0ots@sharklasers.com <ribhav>
2015-01-20 16:45:05 +0530

    home page1.1
    -minor changes

--- _site/data/js/background.js
+++ _site/data/js/background.js
@@ -1,7 +1,7 @@
 var bgcount = 2;
 function changebg() {
     var num =  Math.ceil( Math.random() * bgcount );
-    document.body.style.background = 'url(/freidae/data/backgrounds/backgroundimage'+num+'.jpg)';
+    document.body.style.background = 'url(/freidae/data/backgrounds/BackgroundImage'+num+'.jpg)';
     document.body.style.backgroundRepeat = 'no-repeat';
     document.body.style.backgroundSize = '100% 100%';
     document.body.style.backgroundOrigin = 'border-box';
