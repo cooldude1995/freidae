b1a238f13c073edc89338d3f09dc0239ea0e3066
udbhav1995@gmail.com <ribhav2004>
2015-01-27 14:55:08 +0530

    background 1.3
    -modified script for local machine

--- _site/data/js/background.js
+++ _site/data/js/background.js
@@ -1,6 +1,6 @@
 var bgcount = 57;
 function changebg() {
-    if(document.URL=="http://cooldude1995.github.io/freidae/#home")
+    if(location.pathname=="/freidae/#home"||location.pathname=="/freidae/")
     {var num =  Math.ceil( Math.random() * bgcount );
     document.body.style.background = 'url(/freidae/data/backgrounds/BackgroundImage'+num+'.jpg)';
     document.body.style.backgroundRepeat = 'no-repeat';
@@ -8,11 +8,11 @@ function changebg() {
     document.body.style.backgroundOrigin = 'border-box';
     document.body.style.backgroundAttachment = 'fixed';
     document.body.style.backgroundPosition = 'center center';
-    }   else if(document.URL=="http://cooldude1995.github.io/freidae/products/#prdct"){
+    }   else if(location.pathname=="/freidae/products/#prdct"||location.pathname=="/freidae/products/"){
         document.body.style.background = 'url(#)';
         document.body.style.backgroundColor = '#C9B199';
         
-    }   else if(document.URL=="http://cooldude1995.github.io/freidae/reach_us/#rchus"){
+    }   else if(location.pathname=="/freidae/reach_us/#rchus"||location.pathname=="/freidae/reach_us/"){
         document.body.style.background = 'url(#)';
          document.body.style.backgroundColor = '#986665';
     }
