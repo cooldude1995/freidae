20369de63a5d830fe69a66d262ffd14e2ef2b27d
udbhav1995@gmail.com <ribhav2004>
2015-02-18 13:36:46 +0530

    recaptcha 1.2
    -removed alerts in recptcha

--- _site/data/js/recaptcha.js
+++ _site/data/js/recaptcha.js
@@ -4,7 +4,6 @@ function setupRecaptcha() {
       notice = form.find('#notice');
 
   if (form.length) {
-      alert("abc");
       $.ajax({
         type: 'POST',
         url: contactFormHost + '/send_email',
@@ -13,19 +12,16 @@ function setupRecaptcha() {
         success: function(response) {
           switch (response.message) {
             case 'success':
-                  alert("abcd");
-              form.fadeOut(function() {
+                form.fadeOut(function() {
                 form.html('<h4>' + form.data('success') + '</h4>').fadeIn();
               });
               break;
 
             case 'failure_captcha':
-                   alert("abcde");
               notice.text(notice.data('captcha-failed')).fadeIn();
               break;
 
             case 'failure_email':
-                   alert("abcdf");
               notice.text(notice.data('error')).fadeIn();
           }
         },
