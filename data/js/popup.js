d0007e3e3f49c93c2016264dbf2ef4f5590d8d85
udbhav1995@gmail.com <ribhav2004>
2015-03-01 15:14:35 +0530

    reach us 1.3
    -added message sending intication
    -removed header2 page as it was same with header1
    -moved most of the scripts to bottom

--- data/js/popup.js
+++ data/js/popup.js
@@ -1,14 +1,7 @@
-/*(function($){
-     popup = function() {*/
-        $(document).ajaxSend(function(event, request, settings) {
-            $('#popup,#overlay').show();
-        });
-
-        $(document).ajaxComplete(function(event, request, settings) {
-            $('#popup,#overlay').hide();
-        });
-    $("body").click(function(){
-    $("#overlay, #popup").fadeToggle();
-})
-  /*   };
-})(jQuery);*/
+$("#btn-submit").click(function(){
+    $("#overlay, #popup").fadeIn();
+    return setupRecaptcha();
+});
+$(document).ajaxComplete(function(event, request, settings) {
+    $('#popup,#overlay').fadeOut();
+});
